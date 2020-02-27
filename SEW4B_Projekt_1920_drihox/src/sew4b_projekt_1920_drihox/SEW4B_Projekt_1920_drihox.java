5e35484b0f31dc6eb6ca1ccb0b85d532ffda7b45
drihox15 <drihox15@htl-shkoder.com>
2020-02-27 08:31:14 +0100

    GUI

--- SEW4B_Projekt_1920_drihox/src/sew4b_projekt_1920_drihox/SEW4B_Projekt_1920_drihox.java
+++ SEW4B_Projekt_1920_drihox/src/sew4b_projekt_1920_drihox/SEW4B_Projekt_1920_drihox.java
@@ -1,21 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sew4b_projekt_1920_drihox;
 
-/**
- *
- * @author drihox15
- */
-public class SEW4B_Projekt_1920_drihox {
+import javax.swing.JFrame;
 
-    /**
-     * @param args the command line arguments
-     */
+public class SEW4B_Projekt_1920_drihox {
     public static void main(String[] args) {
-        // TODO code application logic here
+        ProjektGUI g=new ProjektGUI();
+        g.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        g.pack();
+        g.setSize(500,500);
+        g.setVisible(true);
     }
     
 }
